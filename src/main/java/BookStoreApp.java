1e26f90d925964be8e0e43f2428413b0a304559c
Ajmal Hakim <samimajmal@yahoo.com>
2023-12-19 11:34:51 -0500

    add update and delete functionalities

--- src/main/java/BookStoreApp.java
+++ src/main/java/BookStoreApp.java
@@ -64,13 +64,54 @@ public class BookStoreApp {
     }
 
     private static void deleteBook(Scanner sc) {
+        System.out.println("Please enter book id to delete " );
+        int bookId = sc.nextInt();
+        sc.nextLine();
+        Book book = bookDao.getBookById(bookId);
+        if(book == null){
+            System.out.println("No book found ");
+        } else bookDao.deleteBook(bookId);
+        System.out.println("Book deleted successfully ");
     }
 
     private static void updateBook(Scanner sc) {
+        System.out.println("Enter book id to update ");
+        int bookId = sc.nextInt();
+        sc.nextLine();
+        Book book = bookDao.getBookById(bookId);
+        if(book == null){
+            System.out.println("Book not found ");
+        } else {
+            System.out.println("Enter new book title (or press Enter to skip): ");
+            String title = sc.nextLine();
+            System.out.println("Enter new book author (or press Enter to skip): ");
+            String author = sc.nextLine();
+            System.out.println("Enter new book genre (or press Enter to skip): ");
+            String genre = sc.nextLine();
+            System.out.println("Enter new book genre (or press 0 to skip): ");
+            double price = sc.nextDouble();
+            sc.nextLine();
+            book.setTitle(title.isEmpty()? book.getTitle() : title);
+
+//            Boolean emptyAuthor = author.isEmpty();
+//            if(emptyAuthor){
+//                book.setAuthor(book.getAuthor());
+//            } book.setAuthor(author);
+
+            book.setAuthor(author.isEmpty()? book.getAuthor(): author);
+            book.setGenre(genre.isEmpty()? book.getGenre() : genre);
+            book.setPrice(price == 0 ? book.getPrice() : price);
+
+            // update the book
+            bookDao.updateBook(book);
+            System.out.println("Book updated successfully ");
+
+
+        }
     }
 
     private static void addBook(Scanner sc) {
-        System.out.println("PLease enter  Book title ");
+
         String title = sc.nextLine();
         System.out.println("Please enter Book author ");
         String author = sc.nextLine();
